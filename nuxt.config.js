a1c70e8549116aad3213d0557fde92a701ae4ef8
Darko <darko@Darkos-MacBook-Pro.local>
2025-02-24 21:36:04 +0100

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,22 @@
-import { apiEndpoint, repositoryName } from "./slicemachine.config.json";
+import { apiEndpoint, repositoryName } from './slicemachine.config.json';
 import componentsConfig from './config/nuxt/components';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // target: 'static',
-  
-  // generate: {
-  //   fallback: 'index.html',
-  //   dir: '.output/public',
-  // },
+  target: 'static',
 
-  // ssr: false,
+  generate: {
+    fallback: 'index.html',
+    dir: '.output/public',
+  },
+
+  ssr: false,
 
   router: {
     base: '/resume-cv-prismic/',
     options: {
-      scrollBehaviorType: 'smooth'
-    }
+      scrollBehaviorType: 'smooth',
+    },
   },
 
   prismic: {
@@ -24,54 +24,51 @@ export default defineNuxtConfig({
     clientConfig: {
       routes: [
         {
-          type: "page_home",
-          path: "/",
+          type: 'page_home',
+          path: '/',
         },
-        
+
         {
-          type: "page_default",
-          path: "/:uid",
+          type: 'page_default',
+          path: '/:uid',
         },
       ],
-    }
+    },
   },
-
 
   nitro: {
     prerender: {
       crawlLinks: false, // Disable link crawling
       failOnError: false, // Do not fail the build if errors occur
-    }
+    },
   },
 
   components: componentsConfig,
-  
+
   css: ['@/assets/css/tailwind.scss', '@/assets/css/main.scss'],
 
   devtools: { enabled: false, suspensible: false },
 
-  plugins: [
-    './plugins/useSlice.js'
-  ],
-  
+  plugins: ['./plugins/useSlice.js'],
+
   modules: [
-    "@nuxtjs/prismic",
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt",
+    '@nuxtjs/prismic',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
     'nuxt-font-loader',
     '@nuxt/image',
     '@nuxtjs/device',
   ],
-  
+
   fontLoader: {
     local: [
       {
         src: '/fonts/RedHatDisplay.woff2',
         family: 'RedHatDisplay',
-        class: 'RedHatDisplay'
-      }
-    ]
+        class: 'RedHatDisplay',
+      },
+    ],
   },
 
   compatibilityDate: '2025-02-24',
-})
+});
