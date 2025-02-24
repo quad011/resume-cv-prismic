c34b1b7196e9901c5652979c51c89f50667199e9
Darko <darko@Darkos-MacBook-Pro.local>
2025-02-24 10:11:47 +0100

    Updates

--- _nuxt/a77hOAV7.js
+++
@@ -1,1 +0,0 @@
-import c from"./DpJZaQ2l.js";import{ag as s,z as n,o as a,k as i}from"./DOHmo-ZN.js";import"./BDA6FAZo.js";import"./Brfk6Bdo.js";import"./6qCihK2t.js";const g={__name:"index",props:{slice:Object},setup(t){const e=t,{getPrimary:o}=s(e.slice);return(p,m)=>{const r=c;return a(),n(r,{text:i(o)("text")},null,8,["text"])}}};export{g as default};
